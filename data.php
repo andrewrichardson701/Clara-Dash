27c1dcba234f84d6f13f7e78fe1543b2ae0da928
Andrew Richardson <andrewrichardson701@gmail.com>
2026-01-06 08:47:12 +0000

    added some query string options to the data.php url. sensors and ports can be removed

--- data.php
+++ data.php
@@ -8,31 +8,16 @@ $db     = new DB($config['db']);
 
 $devices = [];
 
-// List of devices
+$q_hostname = $_GET['hostname'] ?? null;
+$q_ports    = $_GET['ports']    ?? 1;
+$q_sensors  = $_GET['sensors']  ?? 1;
 
-// $device_list = [
-//     'localhost',
-//     'ar-fw',
-//     'ar-sw',
-//     'jumpbox',
-//     'jumpcli',
-//     'pi-hole1',
-//     'plex',
-//     'proxmox',
-//     'todo',
-//     'torrent',
-//     'truenas',
-//     'web'
-// ];
-
-// for each device get the device_id
-// foreach ($device_list as $device) {  
-//     $device_info = getApiData(['action' => 'device', 'hostname' => $device]);
-
-//     $devices[$device]['device'] = $device_info['device'];
-// }
-
-$device_list = getApiData(['action' => 'devices']);
+// check if a hostname is set, and if it is, search directly for it
+if ($q_hostname) {
+    $device_list = getApiData(['action' => 'devices', 'hostname' => $q_hostname]); 
+} else {
+    $device_list = getApiData(['action' => 'devices']); 
+}
 
 foreach ($device_list['devices'] as $device) {  
     $devices[$device['hostname']]['device'] = $device;
@@ -41,37 +26,46 @@ foreach ($device_list['devices'] as $device) {
 foreach($devices as $device => $device_info) {
     $device_id = $device_info['device']['device_id'];
 
-    $ports   = getApiData(['action' => 'ports', 'device_id' => $device_id])   ?? [];
-    $sensors = getApiData(['action' => 'sensors', 'device_id' => $device_id]) ?? [];
+    // check if ports are required from the api url
+    if ($q_ports == 1) {
+        $ports   = getApiData(['action' => 'ports', 'device_id' => $device_id])   ?? [];
 
-
-    if (array_key_exists('ports', $ports)) {
-        foreach ($ports['ports'] as $i => $port) {
-            // print_r($port);
-            // exit();
-            $graph = getApiData(['action' => 'graph', 'device_id' => $device_id, 'graph_type' => 'port', 'port_id' => $port['port_id'], 'legend' => 'yes']);
-            if (is_array($graph) && array_key_exists('graph', $graph)) {
-                $graph = $graph['graph'];
+        if (array_key_exists('ports', $ports)) {
+            foreach ($ports['ports'] as $i => $port) {
+                // print_r($port);
+                // exit();
+                $graph = getApiData(['action' => 'graph', 'device_id' => $device_id, 'graph_type' => 'port', 'port_id' => $port['port_id'], 'legend' => 'yes']);
+                if (is_array($graph) && array_key_exists('graph', $graph)) {
+                    $graph = $graph['graph'];
+                }
+                $ports['ports'][$i]['graph'] = $graph;
             }
-            $ports['ports'][$i]['graph'] = $graph;
         }
+
+        // add the ports to the array
+        $devices[$device]['ports']   = array_key_exists('ports', $ports) ? $ports['ports'] : [];
     }
 
-    if (array_key_exists('sensors', $sensors)) {
-        foreach ($sensors['sensors'] as $i => $sensor) {
-            // print_r($sensor);
-            // exit();
-            $graph = getApiData(['action' => 'graph', 'device_id' => $device_id, 'graph_type' => 'sensor', 'sensor_id' => $sensor['sensor_id'], 'legend' => 'yes']);
-            if (array_key_exists('graph', $graph)) {
-                $graph = $graph['graph'];
-            }
-            $sensors['sensors'][$i]['graph'] = $graph;
-        }
-    }
-    
+    // check if sensors are required from the api url
+    if ($q_sensors == 1) {
+        $sensors = getApiData(['action' => 'sensors', 'device_id' => $device_id]) ?? [];
 
-    $devices[$device]['ports']   = array_key_exists('ports', $ports) ? $ports['ports'] : [];
-    $devices[$device]['sensors'] = array_key_exists('sensors', $sensors) ? $sensors['sensors'] : [];
+        if (array_key_exists('sensors', $sensors)) {
+            foreach ($sensors['sensors'] as $i => $sensor) {
+                // print_r($sensor);
+                // exit();
+                $graph = getApiData(['action' => 'graph', 'device_id' => $device_id, 'graph_type' => 'sensor', 'sensor_id' => $sensor['sensor_id'], 'legend' => 'yes']);
+                if (array_key_exists('graph', $graph)) {
+                    $graph = $graph['graph'];
+                }
+                $sensors['sensors'][$i]['graph'] = $graph;
+            }
+        }
+        
+        // add the sensors to the array
+        $devices[$device]['sensors'] = array_key_exists('sensors', $sensors) ? $sensors['sensors'] : [];
+    }
+
 }
 
 header('Content-Type: application/json');
