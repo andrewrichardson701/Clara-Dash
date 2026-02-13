b76ea9c60a684f0deff078d0b6c5b7753370ad73
Andrew Richardson <andrewrichardson701@gmail.com>
2026-02-13 08:43:26 +0000

    added a hoddesdon site page

---
+++ includes/hoddesdon.php
@@ -0,0 +1,36 @@
+<div class="container" style="margin-top: 20px">
+    <h1>Hoddesdon - nLighten</h1>
+    <p>Hoddesdon is the primary site for Clara's infrastructure, hosting the majority of our servers and network equipment. It is located in the UK and serves as the main hub for our operations.</p>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col well-nopad bg-dark clickable" style="margin: 5px; background-color: #00759b !important" onclick="(window.location.href='weathermap.php?map=hod_52')">
+            <h4><strong>Suite 52</strong></h4>
+            <p>View the weathermap for Suite 52 in Hoddesdon.</p>   
+        </div>
+        <div class="col well-nopad bg-dark clickable" style="margin: 5px; background-color:rgb(0, 155, 70) !important" onclick="(window.location.href='weathermap.php?map=hod_42')">
+            <h4><strong>Suite 42</strong></h4>
+            <p>View the weathermap for Suite 42 in Hoddesdon.</p>   
+        </div>
+        <div class="col well-nopad bg-dark clickable" style="margin: 5px; background-color:rgb(136, 3, 163) !important" onclick="(window.location.href='weathermap.php?map=hod_21')">
+            <h4><strong>Suite 21</strong></h4>
+            <p>View the weathermap for Suite 21 in Hoddesdon.</p>   
+        </div>
+        <div class="col well-nopad bg-dark clickable" style="margin: 5px; background-color:rgb(211, 4, 131) !important" onclick="(window.location.href='weathermap.php?map=hod_61')">
+            <h4><strong>Suite 61</strong></h4>
+            <p>View the weathermap for Suite 61 in Hoddesdon.</p>   
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col well-nopad bg-dark clickable" style="margin: 5px" onclick="(window.location.href='weathermap.php?map=hod_all')">
+            <h4><strong>All Suites (Combined)</strong></h4>
+            <p>View the combined weathermap for all suites in Hoddesdon.</p>
+        </div>
+        <div class="col well-nopad bg-dark clickable" style="margin: 5px; background-color:rgb(189, 4, 4) !important" onclick="(window.location.href='weathermap.php?map=hod_redstor')">
+            <h4><strong>All Redstor Racks (Combined)</strong></h4>
+            <p>View the weathermap for all Redstor racks in Hoddesdon.</p>
+        </div>
+    </div>
+</div>
