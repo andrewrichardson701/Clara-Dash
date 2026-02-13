cbb9c118d89a072b1abd18259d9643679c18628b
Andrew Richardson <andrewrichardson701@gmail.com>
2026-02-13 10:33:33 +0000

    added styling

--- canvas.php
+++ canvas.php
@@ -1,14 +1,30 @@
 <div id="canvas-wrapper-outer">
+    <div class="container">
+        <h1 id="page-header"></h1>
+    </div>
+    <div id="canvas-wrapper" style="position: relative; max-width:min-content; margin-right:auto; margin-left:auto; display:flex" class="well-nopad bg-dark">
+        <div style="display: inline-block; position: relative;">
+            <canvas id="<?php echo($canvas_id); ?>" width="1800" height="900" style="border:1px solid black; background-color:white">Your browser does not support HTML canvases.</canvas>
+            <div id="coords" class="well-nopad" style="position: absolute; top: 10px; left: 10px; font-family: monospace; color:black; background: rgba(255,255,255,0.8); padding: 4px; border: 1px solid #ccc;" hidden></div>
+            <div id="timestamp" class="well-nopad" style="position: absolute; top: 10px; right: 10px; font-family: monospace; color:black; background: rgba(255,255,255,0.8); padding: 4px; border: 1px solid #ccc;" hidden></div>
+            <div id="dimensions" class="well-nopad" style="font-family: monospace; color:black; background: rgba(255,255,255,0.8); padding: 4px; border: 1px solid #ccc;" hidden></div>
+        </div>
+    </div>
+    <div id="config" class="container well-nopad bg-dark"style="font-family: monospace; padding: 4px; border: 1px solid #ccc; margin-top: 10px; max-width:min-content" hidden>
+    </div>
+</div>
+
+<!-- <div id="canvas-wrapper-outer">
     <h1 id="page-header"></h1>
     <div id="canvas-wrapper" style="position: relative; display: inline-block; max-width:min-content">
         <canvas id="<?php echo($canvas_id); ?>" width="1800" height="900" style="border:1px solid black">Your browser does not support HTML canvases.</canvas>
-        <div id="coords" style="position: absolute; top: 10px; left: 10px; font-family: monospace; background: rgba(255,255,255,0.8); padding: 4px; border: 1px solid #ccc;" hidden></div>
+        <div id="coords"  style="position: absolute; top: 10px; left: 10px; font-family: monospace; background: rgba(255,255,255,0.8); padding: 4px; border: 1px solid #ccc;" hidden></div>
         <div id="timestamp" style="position: absolute; top: 10px; right: 10px; font-family: monospace; background: rgba(255,255,255,0.8); padding: 4px; border: 1px solid #ccc;" hidden></div>
         <div id="dimensions" style="font-family: monospace; background: rgba(255,255,255,0.8); padding: 4px; border: 1px solid #ccc;" hidden></div>
     </div>
     <div id="config" style="font-family: monospace; background: rgba(255,255,255,0.8); padding: 4px; border: 1px solid #ccc; margin-top: 10px; max-width:min-content" hidden>
     </div>
-</div>
+</div> -->
 
 <script>
     var map_file = '<?php echo($map_file); ?>';
--- weathermap.php
+++ weathermap.php
@@ -1,42 +1,63 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+<head>
+    <meta charset="utf-8">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oleo+Script&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet" id="google-font">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="https://adobe-fonts.github.io/source-code-pro/source-code-pro.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.0/css/all.css">
+</head>
+<body>
 
-include_once 'includes/functions.php';
-
-$continue = $map_dir = $map_file = false;
-
-if (!isset($map)) {
-    $map = $_GET['map'] ?? null;
-}
-
-if ($map) {
-    $map_name = $map;
-    $map_file = $map.'.json';
-    $map_dir  = './maps';
-    if (search_file(getcwd().DIRECTORY_SEPARATOR.$map_dir, $map_file)) {
-        $continue = true;
-    } 
-    $map_file = $map_dir.DIRECTORY_SEPARATOR.$map_file;
-    $map_data = json_decode(file_get_contents($map_file), true);
-    $canvas_id = $map_data['Config']['canvas_id'];
-}
-
-// error output.
-if (!$continue) {
-    ?><strong>Unable to find map <?php if($map_dir && $map_file) { echo('at: '.getcwd().DIRECTORY_SEPARATOR.$map_dir.$map_file); } ?></strong>
     <?php
-    exit();
-}
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
-// add in the canvas to load the weathermap
-include 'canvas.php';
+    include_once 'includes/functions.php';
 
-// Temporarily include the README
-?>
-<div id="readme" style="font-family: monospace; background: rgba(255,255,255,0.8); padding: 4px; border: 1px solid #ccc; margin-top:10px">
-<?php
-include 'maps/map_README.md';
-?>
-</div>
+    $continue = $map_dir = $map_file = false;
+
+    if (!isset($map)) {
+        $map = $_GET['map'] ?? null;
+    }
+
+    if ($map) {
+        $map_name = $map;
+        $map_file = $map.'.json';
+        $map_dir  = './maps';
+        if (search_file(getcwd().DIRECTORY_SEPARATOR.$map_dir, $map_file)) {
+            $continue = true;
+        } 
+        $map_file = $map_dir.DIRECTORY_SEPARATOR.$map_file;
+        $map_data = json_decode(file_get_contents($map_file), true);
+        $canvas_id = $map_data['Config']['canvas_id'];
+    }
+
+    // error output.
+    if (!$continue) {
+        ?><strong>Unable to find map <?php if($map_dir && $map_file) { echo('at: '.getcwd().DIRECTORY_SEPARATOR.$map_dir.$map_file); } ?></strong>
+        <?php
+        exit();
+    }
+    ?>
+    <div id="canvas-content">
+    <?php
+        // add in the canvas to load the weathermap
+        include 'canvas.php';
+
+        // Temporarily include the README
+        ?>
+        <div id="readme" class="container well-nopad bg-dark" style="font-family: monospace; background: rgba(255,255,255,0.8); padding: 4px; border: 1px solid #ccc; margin-top:10px">
+            README file: 'maps/map_README.md' <br><br>
+            <?php
+            include 'maps/map_README.md';
+            ?>
+        </div>
+    </div>
+</body>
