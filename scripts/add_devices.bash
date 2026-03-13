d4cdcfa7ae26ed4716c8b3d776c57d8389868ae6
Andrew Richardson <andrewrichardson701@gmail.com>
2026-03-13 12:29:00 +0000

    added the LD5 map

--- scripts/add_devices.bash
+++ scripts/add_devices.bash
@@ -4,5 +4,5 @@ while IFS= read -r hostname; do
     # skip empty lines
     [[ -z "$hostname" ]] && continue
 
-    ./add_device.php "$hostname" h6of21oju v2c 161 udp
+    ../../../add_device.php "$hostname" h6of21oju v2c 161 udp
 done < devices.txt
